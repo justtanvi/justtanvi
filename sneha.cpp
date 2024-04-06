f83b92bd3fb90f475e541d1035ee8db508d9eb0a
Tanvi <tsawant711@gmail.com>
2024-04-06 18:28:49 +0530

    new

--- sneha.cpp
+++ sneha.cpp
@@ -6,9 +6,16 @@ void add(int a , int b)
 	cout<<"Addition :"<<a+b<<endl;
 }
 
+void mul(int x , int y)
+{
+	cout<<"Multiplication :"<<x*y<<endl;
+}
+
 int main()
 {
 	add(5,7);
+	mul(6,8);
+	return 0;
 	
 	
 }
